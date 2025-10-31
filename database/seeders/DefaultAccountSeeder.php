83c6eb8ac0e63026eb451817e3171e5fa27aae62
MD. ALA UDDIN <md.alauddin.nstu@gmail.com>
2025-10-31 22:28:39 +0600

    feat(seeder): add account seeder

---
+++ database/seeders/DefaultAccountSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Account;
+use Illuminate\Database\Seeder;
+
+class DefaultAccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (Account::count() === 0) {
+            Account::create(['name' => 'Bank', 'balance' => 0.00]);
+            Account::create(['name' => 'Cash', 'balance' => 0.00]);
+        }
+    }
+}
