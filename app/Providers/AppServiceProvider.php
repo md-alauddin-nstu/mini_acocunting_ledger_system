d7d42ccd078ec1e0ca945cf546ac6eec81cb8645
MD. ALA UDDIN <md.alauddin.nstu@gmail.com>
2025-10-31 22:12:13 +0600

    feat(facade): add Ledger facade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\LedgerService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton('ledger', function ($app) {
+            return new LedgerService;
+        });
     }
 
     /**
